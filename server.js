eaf0c16a8f4adccceb1fc4e99a152941c5819150
Vaibhav <vaibhav.mishra0509@gmail.com>
2021-04-10 12:45:48 +0530

    final push

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path =  require('path')
+const Sentiment = require('sentiment');
+const port = 3000
+const app  = express()
+app.use(express.static(path.join(__dirname,'public')))
+
+
+app.get('/', function(req, res)
+{
+    res.sendFile(path.join(__dirname, 'index.html'))
+
+})
+
+app.get('/emotion', function(req, res)
+{
+    const sentiment = new Sentiment()
+    const text = req.query.text // this returns our request query "text"
+    const score = sentiment.analyze(text);
+  
+    res.send(score)
+})
+
+app.listen(port, function(){
+    console.log(`Listening to port: ${port}!`)
+})
+
+
